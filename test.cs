857c999dec1376fcdb8b9ea25c182d8163410566
Jon Erickson <erickson.jon@gmail.com>
2014-06-16 15:31:20 -0400

    Sample code to create .cctor

---
+++ test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testns
+{
+    class test
+    {
+        static test()
+        {
+            Console.WriteLine("First");
+        }
+
+        public test()
+        {
+            Console.WriteLine("Second");
+        }
+     
+        public static void Main(String[] args)
+        {
+            test t = new test();
+            Console.Write("Hello");
+        }
+    }
+}
